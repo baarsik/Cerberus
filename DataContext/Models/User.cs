44d52a457d82ece921025b8322cc3d51fa0a29f9
Mark Malakhouski <barsik@cerberus.by>
2018-03-07 12:42:22 +0300

    Fix VCS

---
+++ DataContext/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataContext.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(32)]
+        public string DisplayName { get; set; }
+
+        [MaxLength(24)]
+        public virtual string FirstName { get; set; }
+
+        [MaxLength(32)]
+        public virtual string LastName { get; set; }
+
+        [MaxLength(32)]
+        public virtual string MiddleName { get; set; }
+
+        [MaxLength(100)]
+        public virtual string LockoutReason { get; set; }
+
+        [Required]
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Avatar { get; set; } = "noavatar";
+
+        [MaxLength(8)]
+        public virtual string Culture { get; set; }
+    }
+}
