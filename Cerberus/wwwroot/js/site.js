cc63a4ddd149fc769323ee52a7a0f4d1de364300
markmolohovskij <barsik@cerberus.by>
2019-03-04 12:45:30 +0300

    Style update - Added panel top label style (purpose: quotations in Web Novels)

--- Cerberus/wwwroot/js/site.js
+++ Cerberus/wwwroot/js/site.js
@@ -1,1 +1,32 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function() {
+    updateQuotationPanelStyle();
+});
+
+function updateQuotationPanelStyle() {
+    $('.panel').each(function(i) {
+        if ($(this).children('.panel-top-label').length === 0)
+            return true;
+
+        var additiveMargin = $(this).children('.panel-top-label').first().height() / 2;
+        $(this).css('position', 'relative');
+        $(this).css('top', function(index, currentValue) {
+            var currentMargin = parseInt(currentValue, 10);
+            return currentMargin + additiveMargin + 'px';
+        });
+        $(this).css('margin-bottom', function(index, currentValue) {
+            var currentMargin = parseInt(currentValue, 10);
+            return currentMargin + additiveMargin + 'px';
+        });
+
+        var firstActualChild = $(this).children(':not(.panel-top-label)').first();
+        firstActualChild.css('position', 'relative');
+        firstActualChild.css('top', function(index, currentValue) {
+            var currentMargin = parseInt(currentValue, 10);
+            return currentMargin - additiveMargin + 'px';
+        });
+        firstActualChild.css('margin-bottom', function(index, currentValue) {
+            var currentMargin = parseInt(currentValue, 10);
+            return currentMargin - additiveMargin + 'px';
+        });
+    });
+}
