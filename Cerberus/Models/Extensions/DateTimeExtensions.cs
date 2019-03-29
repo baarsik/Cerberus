c16b6ee5eb42b010513741dc69d3d2f7c7b20bd6
markmolohovskij <barsik@cerberus.by>
2019-03-29 17:00:20 +0300

    Minor /wn/read/ update - Added 'Upload Date' to the webnovel chapter read view - Date display format now supports 'Yesterday' value - Minor rules update

--- Cerberus/Models/Extensions/DateTimeExtensions.cs
+++ Cerberus/Models/Extensions/DateTimeExtensions.cs
@@ -5,8 +5,10 @@ namespace Cerberus.Models.Extensions
     public static class DateTimeExtensions
     {
         public static string ToFormattedString(this DateTime dateTime)
-            => dateTime.Date == DateTime.Now.Date
+            => dateTime.Date == DateTime.Today
                 ? "Today"
-                : dateTime.ToString(Constants.Misc.DateFormat);
+                : dateTime.Date == DateTime.Today - TimeSpan.FromDays(1)
+                    ? "Yesterday"
+                    : dateTime.ToString(Constants.Misc.DateFormat);
     }
 }
