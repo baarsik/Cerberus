a9af962e6560c56e49116bb0fc83ce1cf68f40f8
markmolohovskij <barsik@cerberus.by>
2019-04-26 17:50:06 +0300

    Web novel localization update (2) - Added partial localization - Date no longer displayes "Today" and "Yesterday" values - Updated several packages

--- Cerberus/Models/Extensions/DateTimeExtensions.cs
+++ Cerberus/Models/Extensions/DateTimeExtensions.cs
@@ -5,10 +5,6 @@ namespace Cerberus.Models.Extensions
     public static class DateTimeExtensions
     {
         public static string ToFormattedString(this DateTime dateTime)
-            => dateTime.Date == DateTime.Today
-                ? "Today"
-                : dateTime.Date == DateTime.Today - TimeSpan.FromDays(1)
-                    ? "Yesterday"
-                    : dateTime.ToString(Constants.Misc.DateFormat);
+            => dateTime.ToString(Constants.Misc.DateFormat);
     }
 }
