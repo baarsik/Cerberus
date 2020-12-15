0f009aa0afeb8eea8c7d5b830d8469d6e7a75801
markmolohovskij <barsik@cerberus.by>
2020-12-15 11:50:50 +0900

    Added avatar tag helper

---
+++ Cerberus/Models/TagHelpers/AvatarTagHelper.cs
@@ -0,0 +1,24 @@
+using Cerberus.Models.Extensions;
+using DataContext.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Cerberus.Models.TagHelpers
+{
+    [HtmlTargetElement("avatar", TagStructure = TagStructure.WithoutEndTag)]
+    public class AvatarTagHelper : TagHelper
+    {
+        public ApplicationUser User { get; set; }
+        
+        public string Class { get; set; }
+        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {   
+            output.SuppressOutput();
+            var avatarHtml = User.Avatar == "noavatar"
+                ? "<i class=\"fas fa-user\"></i>"
+                : $"<img src=\"{User.GetAvatarUrl()}\" alt=\"Avatar\"/>";
+            output.Content.AppendHtmlLine($"<div class=\"{Class}\">{avatarHtml}</div>");
+        }
+    }
+}
